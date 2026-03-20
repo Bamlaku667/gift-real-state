412dd3d2c3b80cb0d2f0b5537cb5e256550ff731
bamlak4774 <bamlaku.hiruy@eaglelionsystems.com>
2026-03-20 14:06:42 +0300

    add gift website

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("176c9baa1a95039a")
+/******/ 		__webpack_require__.h = () => ("45e32a922acc4cdd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "994c10e183bd5cf9"; }
+/******/ 		__webpack_require__.h = function() { return "660b363744e2f7f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
